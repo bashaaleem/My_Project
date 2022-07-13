71f2cf727a618caf766d4b127ace8abedaeb921c
bashaaleem <absh90@outlook.com>
2022-07-13 15:08:45 +0530

    First Commit

---
+++ src/main/java/org/example/Bike.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Bike {
+
+    public static void main(String[] args){
+        int a=5;
+        char m = 'j';
+
+        System.out.println("Enter the string : "+a+"char : "+m );
+    }
+}
